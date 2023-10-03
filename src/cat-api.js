728234510b41a726ea47407ba8a7fe6152cdd835
Julia <129061308+JuliaStavychna@users.noreply.github.com>
2023-10-03 17:27:05 +0300

    update

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,23 +1,42 @@
+import axios from 'axios';
+
 const URL = 'https://api.thecatapi.com/v1';
 const API_KEY =
   'live_G2G0cDpvIYgN7fza4dikrLECKEmYCFX6embSg8vuddUyAdl7k2W6CeocDW9Id921';
 
 export function fetchBreeds() {
-  return fetch(`${URL}/breeds?api_key=${API_KEY}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  return axios
+    .get(`${URL}/breeds`, {
+      params: {
+        api_key: API_KEY,
+      },
+    })
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error(response.status);
+      }
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error(error.message);
+    });
 }
 
 export function fetchCatByBreed(breedId) {
-  return fetch(
-    `${URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  return axios
+    .get(`${URL}/images/search`, {
+      params: {
+        api_key: API_KEY,
+        breed_ids: breedId,
+      },
+    })
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error(response.status);
+      }
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error(error.message);
+    });
 }
